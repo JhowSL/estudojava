f1194000a730686be443658f0b50c46358a050e8
Jonathan Souza Lima <jon.1lima@hotmail.com>
2022-11-23 03:26:14 -0300

    JhowSL
    Gerando o arquivo base

---
+++ src/main/java/com/br/estduos/estudojava/EstudojavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.estduos.estudojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/estduos/estudojava/EstudojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.estduos.estudojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
