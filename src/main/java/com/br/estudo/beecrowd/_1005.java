d6946717620d2beeb11ccb82a95e555abd250529
JhowSL <jon.1lima@hotmail.com>
2023-04-11 01:21:17 -0300

    ajuste -> pasta 3

--- src/main/java/com/br/estudo/beecrowd/_1005.java
+++ src/main/java/com/br/estudo/beecrowd/_1005.java
@@ -27,11 +27,11 @@ public class _1005 {
     }
 
     // resolução aceita
-//    public static void main2(String[] args) throws IOException {
-//        Scanner leitor = new Scanner(System.in);
-//        double a = leitor.nextDouble();
-//        double b = leitor.nextDouble();
-//        double media = ((3.5 * a) + (7.5 * b)) / 11;
-//        System.out.println(String.format("MEDIA = %.5f", media));
+    public static void main2(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        double a = leitor.nextDouble();
+        double b = leitor.nextDouble();
+        double media = ((3.5 * a) + (7.5 * b)) / 11;
+        System.out.println(String.format("MEDIA = %.5f", media));
     }
 }
